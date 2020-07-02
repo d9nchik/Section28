b24c6665eaa73e5839e9d48adba69692da43ca34
d9nich <dan.galaiko@gmail.com>
2020-07-02 18:42:05 +0300

    Visualisation of graph has been done.

---
+++ src/com/d9nich/Displayable.java
@@ -0,0 +1,9 @@
+package com.d9nich;
+
+public interface Displayable {
+    double getX(); // Get x-coordinate of the vertex
+
+    double getY(); // Get y-coordinate of the vertex
+
+    String getName(); // Get display name of the vertex
+}
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module Section {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.junit.jupiter.api;
+
+    opens com.d9nich;
+}
