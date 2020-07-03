94025a79da58cd7ad97fee02e9e315d907125999
d9nich <dan.galaiko@gmail.com>
2020-07-03 17:23:34 +0300

    Exercise28.3 has been done. A new algorithm without using recursion. Implemented by defining a new class named UnweightedGraphWithNonrecursiveDFS that extends UnweightedGraph and overriding the dfs method.

---
+++ src/com/d9nich/UnweightedGraphWithNonRecursiveDFS.java
@@ -0,0 +1,53 @@
+package com.d9nich;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UnweightedGraphWithNonRecursiveDFS<V> extends UnweightedGraph<V> {
+    public UnweightedGraphWithNonRecursiveDFS() {
+    }
+
+    public UnweightedGraphWithNonRecursiveDFS(V[] vertices, int[][] edges) {
+        super(vertices, edges);
+    }
+
+    public UnweightedGraphWithNonRecursiveDFS(List<V> vertices, List<Edge> edges) {
+        super(vertices, edges);
+    }
+
+    @Override
+    public SearchTree dfs(int v) {
+        List<Integer> searchOrder = new ArrayList<>();
+        int[] parent = new int[vertices.size()];
+        // Initialize parent[i] to −1
+        Arrays.fill(parent, -1);
+        // Mark visited vertices
+        boolean[] isVisited = new boolean[vertices.size()];
+        // Recursively search
+
+        final ArrayList<Integer> stack = new ArrayList<>();
+        stack.add(v);
+        while (!stack.isEmpty()) {
+            final Integer index = stack.get(stack.size() - 1);
+            if (!isVisited[index]) {
+                isVisited[index] = true;
+                searchOrder.add(index);
+            }
+            for (Edge edge : neighbors.get(index)) {
+                final int neighbour = edge.v;
+                if (!isVisited[neighbour]) {
+                    parent[neighbour] = index;
+                    stack.add(neighbour);
+                    break;
+                }
+            }
+
+            if (stack.get(stack.size() - 1).equals(index))
+                stack.remove(stack.size() - 1);
+        }
+
+        // Return a search tree
+        return new SearchTree(v, parent, searchOrder);
+    }
+}
---
+++ src/com/d9nich/UnweightedGraphWithNonRecursiveDFSTest.java
@@ -0,0 +1,5 @@
+package com.d9nich;
+
+class UnweightedGraphWithNonRecursiveDFSTest extends UnweightedGraphTest {
+    Graph<String> graph1 = new UnweightedGraphWithNonRecursiveDFS<>(vertices, edges);
+}
